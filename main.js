907bc5c9e63e6c3547f5c01aff194b87836aa6dd
andreyanez <andre.yanez22@gmail.com>
2023-02-01 14:36:04 -0500

    basic popup and config page

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-const keyword = 'trash';
+function updateView() {
+	const keyword = localStorage.getItem('keyword');
+	if (!keyword) return;
+	console.log(keyword);
+}
 
-// Alternative to DOMContentLoaded event
-document.onreadystatechange = () => {
-	if (document.readyState === 'complete') {
-		setInterval(() => {
-			const content = document.getElementsByTagName('span');
-			for (let index = 0; index < content.length; index++) {
-				const element = content[index];
-				if (element.innerText === keyword) element.innerText = 'covered';
-			}
-		}, 1000);
-	}
-};
+updateView();
+
+// 	setInterval(() => {
+// 		const content = document.getElementsByTagName('span');
+// 		for (let index = 0; index < content.length; index++) {
+// 			const element = content[index];
+// 			if (element.innerText === value) element.innerText = 'covered';
+// 		}
+// 	}, 1000);
+
+// document.onreadystatechange = () => {
+// 	if (document.readyState === 'complete') {
+// 	}
+// };
 
 // COULD IMPLEMENT THE FUNCTION ON SCROLL RATHEN THAN IN AN INTERVAL
-
-// cloaked
